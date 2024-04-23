6bd790951c5cb887f05ac594232b75e7d67e5954
Carlos Valencia Sánchez <167770750+valen28030@users.noreply.github.com>
2024-04-23 18:45:07 +0200

    Add files via upload

---
+++ wateke/php/conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+	$Servidor = "127.0.0.1";
+	$Usuario = "root";
+	$Password = "";
+	$bd = "wateke";
+
+
+
+	/*$Servidor = "sql113.infinityfree.com";
+	$Usuario = "if0_35442468";
+	$Password = "x9r77ibwXsiP";
+	$bd = "if0_35442468_base_datos1";*/
+?>
